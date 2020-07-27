16fe4f166ac4e2d78767568448574b0925888c4b
PRAVEEN G <praveeng@PRAVEENs-MacBook-Pro-2.local>
2020-07-27 15:09:05 -0700

    updated the teacups

--- src/main/java/com/tomgregory/ThemeParkApplication.java
+++ src/main/java/com/tomgregory/ThemeParkApplication.java
@@ -18,7 +18,7 @@ public class ThemeParkApplication  {
         return (args) -> {
             repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
             repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
-            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant red  tea-cup", 21, 4));
+            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant blue  tea-cup", 21, 4));
         };
     }
 }
