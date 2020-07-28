ff4afad96c6261055b81ccaee7eb4540c67085fd
PRAVEEN G <praveeng@PRAVEENs-MacBook-Pro-2.local>
2020-07-28 09:29:54 -0700

    initial commit

--- src/main/java/com/tomgregory/ThemeParkApplication.java
+++ src/main/java/com/tomgregory/ThemeParkApplication.java
@@ -18,7 +18,7 @@ public class ThemeParkApplication  {
         return (args) -> {
             repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
             repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
-            repository.save(new ThemeParkRide("Teacups", "Spinning in a giant green red tea-cup", 22, 611));
+            repository.save(new ThemeParkRide("Teacups", "Spinning in a giant green red tea-cup with a handle", 22, 611));
         };
     }
 }
